fda6c271af3166d89bc815edf137b24024b13e0d
SMILE-KILLER10 <85184491+SMILE-KILLER10@users.noreply.github.com>
2021-07-20 15:48:45 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,21 +1,21 @@
 class Translation(object):
     START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
 
-<b>Bot Maintained By: @princedavitt</b> \n
+<b>Bot Maintained By: @smile_killer_010</b> \n
 <b>Do /help for more...</b> \n
 """
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
-    BANNED_USER_TEXT = "Sorry!! But My Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Go To : @MalayaliTGinfos For more Details.. " 
+    BANNED_USER_TEXT = "Sorry!! But My Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Go To : @NAZRIYAOFFTOPIC For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "Go to @princedavitt and ask."
+    UPGRADE_TEXT = "Go to @NAZRIYAOFFTOPIC and ask."
     DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
     UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me Start> @Cinemaglitz Join> @Cinemaglitz Support> @Cglitz ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me Start> @NAZRIYAOFFTOPIC Join> @FILMCORNERMM Support> @NAZRIYAONTOPIC ❤️**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "Go To @Cinemaglitz "
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Go to @Princedavitt "
+    NOT_AUTH_USER_TEXT = "Go To @NAZRIYAOFFTOPIC "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Go to @SMILE_KILLER_010 "
     SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅ This Is Permanent Thumbnail Until</b> /deletethumbnail ❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
@@ -27,12 +27,12 @@ class Translation(object):
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n Click here to know about me https://t.me/princedavitt.</b>"""
+    ABOUT_ME = """<b>Meh : \n Click here to know about me https://t.me/SMILE_KILLER_010.</b>"""
     HELP_USER = """Ohh You Want Help?? 😅
     
 1.🔹 <b>Send me any Telegram File.</b> \n
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
 3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
 4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n \n <b>Made With Love By: @Cinemaglitz</b>
-5.🔹 <b> [Click here](https://t.me/princedavitt) to know more informations </b>   \n"""
+5.🔹 <b> [Click here](https://t.me/smile_killer_010) to know more informations </b>   \n"""
 
